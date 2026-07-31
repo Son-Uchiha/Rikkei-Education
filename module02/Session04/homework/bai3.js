16edf88c549d2a6a6936f92348b42280377a07e1
Nguyen Duy Son <uchihason5@gmail.com>
2026-07-31 14:46:20 +0700

    done bai 3

---
+++ module02/Session04/homework/bai3.js
@@ -0,0 +1,13 @@
+// 1. Tạo một mảng rỗng tên là students
+let students = [];
+
+// 2. Sử dụng push() để thêm 3 tên sinh viên vào cuối mảng
+students.push("An", "Bình", "Cường");
+
+// 3. Sử dụng unshift() để thêm 1 tên sinh viên vào đầu mảng
+students.unshift("Hoàng");
+
+// 4. Sử dụng pop() để xóa sinh viên cuối cùng và in mảng hiện tại ra Console
+students.pop();
+
+console.log("Danh sách sinh viên hiện tại:", students);
